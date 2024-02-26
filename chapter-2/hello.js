9894a3e212cb00ad80a33de462a477bc6a82bc8a
NazmushSakib <sakib.dev01@.com>
2024-02-26 22:21:49 +0600

    upto video 008 completed

---
+++ chapter-2/hello.js
@@ -0,0 +1,30 @@
+
+
+//Video no. 005 First Code
+console.log('Hello World')
+console.log('I love Bangladesh')
+console.log('45')
+console.log(609.35)
+console.log('My Favorite no. : 36')
+console.log('My Favorite no. : ' + 8)
+console.log(9+9)
+console.log('9'+9)
+
+//Video no. 006 Variable 
+
+var  name = 'HM Nayem'
+var age = 26
+
+
+console.log(name +' knows JavaScript')
+console.log('His age is only ' + age)
+console.log(name +' is creating a JavaScript Course for All')
+console.log('But his age is only ' + age)
+
+//Video no. 007  | JS Reserved Words
+var mathNumber = 10
+var accountNumberDetailsId =104
+var account_number_details_id = 104
+
+//Video no.008. JS Data Types
+//Theory based class. see notion
